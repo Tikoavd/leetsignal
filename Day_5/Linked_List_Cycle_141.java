4077d5088e2fdc3560ba3de8540446fc1b1d8c6f
Tikoavd <114668015+Tikoavd@users.noreply.github.com>
2022-09-30 11:31:29 +0400

    Create Linked_List_Cycle_141.java

---
+++ Day_5/Linked_List_Cycle_141.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> table = new HashSet<>();
+        
+        while(head != null)
+        {
+            if(table.contains(head)) return true;
+            table.add(head);
+            head = head.next;
+        }
+        
+        return false;
+    }
+}
